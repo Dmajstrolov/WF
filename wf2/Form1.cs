2a2b0330908c8529c1f23e497030fcf3c800643b
Elias Bengtsson <eliben0011@skynet.halmstad.se>
2019-08-30 13:17:09 +0200

    fil

--- wf2/Form1.cs
+++ wf2/Form1.cs
@@ -16,5 +16,10 @@ namespace wf2
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hej");
+        }
     }
 }
